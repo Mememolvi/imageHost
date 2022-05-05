fab878be88e01972c2449f854cd5e9d33a049753
neoAli <darali7575@gmail.com>
2022-05-05 21:57:36 +0530

    Initial commit

---
+++ src/main/java/com/image/ImageHostApplication.java
@@ -0,0 +1,13 @@
+package com.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageHostApplication.class, args);
+	}
+
+}
