e41b3baff01132961de4844b292de4ceeb275b30
neoAli <darali7575@gmail.com>
2022-05-07 19:33:19 +0530

    GET request response changed to Image

--- src/main/java/com/image/api/ImageAPI.java
+++ src/main/java/com/image/api/ImageAPI.java
@@ -4,10 +4,12 @@ import com.image.dto.ImageDTO;
 import com.image.service.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,14 +18,21 @@ public class ImageAPI {
     @Autowired
     private ImageService imageService;
 
-    @GetMapping(value = "/images/{id}")
-    public ResponseEntity<ImageDTO> getImage(@PathVariable Integer id) throws Exception {
+    @GetMapping(value = "/images/byte/{id}")
+    public ResponseEntity<ImageDTO> getImageAsByte(@PathVariable Integer id) throws Exception {
         ImageDTO imageDTO = imageService.getImage(id);
         return new ResponseEntity<>(imageDTO, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/images/all/{pageNumber}")
-    public ResponseEntity<List<ImageDTO>> getAllImages(@PathVariable Integer pageNumber) throws Exception {
+    @GetMapping(value = "/images/{id}",
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable Integer id) throws Exception {
+        ImageDTO imageDTO = imageService.getImage(id);
+        return new ResponseEntity<>(imageDTO.getPicByte(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/images/byte/all/{pageNumber}")
+    public ResponseEntity<List<ImageDTO>> getAllImagesAsByte(@PathVariable Integer pageNumber) throws Exception {
         List<ImageDTO> imageDTOList = imageService.findAll(pageNumber);
         return new ResponseEntity<>(imageDTOList, HttpStatus.OK);
     }
